ed3053e42d10e62c018ba3cca7edb803cf1edac0
elly-agent <japantea.chakou@gmail.com>
2026-07-26 23:55:41 +0900

    feat: 服装チャプターに「海で遊ぶときの心構え」セクションを追加

--- script.js
+++ script.js
@@ -662,6 +662,21 @@ function renderClothing(data) {
   });
 }
 
+function renderSeaPlayTips(data) {
+  const tips = data.seaPlayTips;
+  const imageWrap = document.getElementById("sea-play-tips-image");
+  imageWrap.appendChild(makeImg(tips.image, "海で遊ぶ様子"));
+
+  const list = document.getElementById("sea-tips-list");
+  tips.items.forEach((item) => {
+    const card = el("div", "sea-tip-item reveal");
+    card.innerHTML =
+      "<span class='sea-tip-item-title'>" + item.title + "</span>" +
+      "<span class='sea-tip-item-text'>" + item.text + "</span>";
+    list.appendChild(card);
+  });
+}
+
 // --- Chapter 9: 持ち物チェックリスト（ローカル保存対応） ---
 const CHECKLIST_STORAGE_KEY = "baliTour2026_checklist";
 
@@ -1138,6 +1153,7 @@ function renderAllChapters(data) {
   renderCurrency(data);
   renderBargain(data);
   renderClothing(data);
+  renderSeaPlayTips(data);
   renderPacking(data);
   renderItinerary(data);
   renderHotels(data);
